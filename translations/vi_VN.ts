5b599dc3c16feffea02827f5317e091405309b17
Victor Tran <vicr12345@gmail.com>
2018-11-16 12:02:01 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 94.4% (17 of 18 strings)
    
    Translation: theShell/theShell Platform Integration
    Translate-URL: http://translate.vicr123.com/projects/theshell/ts-qtplatform/vi/

--- translations/vi_VN.ts
+++ translations/vi_VN.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>FileDialog</name>
     <message>
@@ -11,17 +11,17 @@
     <message>
         <location filename="../filedialog.ui" line="25"/>
         <source>Select a file</source>
-        <translation type="unfinished"></translation>
+        <translation>Chọn tệp</translation>
     </message>
     <message>
         <location filename="../filedialog.ui" line="47"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Hủy</translation>
     </message>
     <message>
         <location filename="../filedialog.ui" line="57"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
 </context>
 <context>
@@ -29,73 +29,73 @@
     <message>
         <location filename="../messagedialog.cpp" line="49"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="60"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Có</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="71"/>
         <source>Yes to All</source>
-        <translation type="unfinished"></translation>
+        <translation>Có cho tất cả</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="82"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Không</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="93"/>
         <source>No to All</source>
-        <translation type="unfinished"></translation>
+        <translation>Không cho tất cả</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="104"/>
         <location filename="../messagedialog.cpp" line="205"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Hủy</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="115"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Lưu</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="126"/>
         <source>Save All</source>
-        <translation type="unfinished"></translation>
+        <translation>Lưu tất cả</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="137"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Mở</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="148"/>
         <source>Discard</source>
-        <translation type="unfinished"></translation>
+        <translation>Vứt đi</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="159"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Đóng</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="170"/>
         <source>Abort</source>
-        <translation type="unfinished"></translation>
+        <translation>Bỏ ngay</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="181"/>
         <source>Retry</source>
-        <translation type="unfinished"></translation>
+        <translation>Thử lại</translation>
     </message>
     <message>
         <location filename="../messagedialog.cpp" line="192"/>
         <source>Ignore</source>
-        <translation type="unfinished"></translation>
+        <translation>Bỏ qua</translation>
     </message>
 </context>
 </TS>
